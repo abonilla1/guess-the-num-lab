a775a9dde8ad8545da1706199fce2a0026403466
abonilla1 <amariabrown015@gmail.com>
2020-11-09 10:58:25 -0600

    complete/working

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
 const game = {
+    // initialize state
     title: 'Guess the Number!',
-    biggestNum: null,
-    smallestNum: null,
+    biggestNum: 0,
+    smallestNum: 0,
     secretNum: null,
     prevGuesses: [],
-    getGuess: function() {
-        let guess = parseInt(prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum} to guess the Secret Number!`));
-        while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
-            guess = parseInt(prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum} to guess the Secret Number!`));
-        }
-        return guess;
-    },
-    render: function(x){
-        if (x === this.secretNum) {
-            alert(`Congrats! You guessed the number in ${prevGuesses.length} guesses!`);
-        } else if (x > this.secretNum) {
-            alert(`Your guess is too high!`);
-            let playerPreviousGuesses = prevGuesses.toLocaleString(',');
-            alert(`Previous guesses ${playerPreviousGuesses}`);
-        } else if (x < this.secretNum) {
-            alert(`Your guess is too low!`);
-            playerPreviousGuesses = prevGuesses.toLocaleString(',');
-            alert(`Previous guesses ${playerPreviousGuesses}`);
-        }
-    },
     play: function() {
         this.biggestNum = parseInt(prompt(`Enter the top number for your guessing range!`));
         this.smallestNum = parseInt(prompt(`Enter the bottom number for your guessing range!`));
         this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum +1)) + this.smallestNum;
-        let playerGuess = this.getGuess();
-        this.prevGuesses.push(playerGuess);
-        while (playerGuess !== this.secretNum) {
+        let playerGuess = NaN;
+        while (playerGuess !== this.secretNum) { 
             playerGuess = this.getGuess();
-            this.render(playerGuess);   
-            if (playerGuess === this.secretNum) return; 
-        }    
+            this.prevGuesses.push(playerGuess);
+            this.render(playerGuess);
+            if (playerGuess === this.secretNum) return
+        }
+    },
+    getGuess: function() {
+        let guess = NaN;
+        while (isNaN(guess) || guess < this.smallestNum || guess > this.biggestNum) {
+           guess = parseInt(prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum} to guess the Secret Number!`));  
+        }
+        return guess;
+    },
+    render: function(x){
+        let playerPreviousGuesses = this.prevGuesses.join(',');
+        if (x === this.secretNum) {
+            alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`);
+        } 
+        else if (x > this.secretNum) {
+            alert(`Your guess is too high! Previous guesses ${playerPreviousGuesses}`);
+        } 
+        else if (x < this.secretNum) {
+            alert(`Your guess is too low! Previous guesses ${playerPreviousGuesses}`);
+        } 
+        else {
+            alert('Critical error');
+        }       
     }
-  };
-
-  game.play();
+};
+  
+game.play();
 
 
